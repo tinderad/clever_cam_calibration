1350552bd9f2bf9c4a264a6ae1368f96929ef800
tinderad <44276006+tinderad@users.noreply.github.com>
2018-12-23 22:45:20 +0300

    1.0

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='clevercamcalib',
-    version='alpha 1.0',
-    packages=['clevercamcalib'],
-    url='clever.copterexpress.com',
-    license='',
-    author='copterexpress',
-    author_email='vovalekhterev@gmail.com',
-    description='Camera calibration package'
+    name='clever_cam_calibration',
+    version='1.0',
+    packages=['clevercamcalib']
 )
