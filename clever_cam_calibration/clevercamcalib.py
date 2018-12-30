a073040c2e5d43127e228cee0885fcb50221f462
tinderad <44276006+tinderad@users.noreply.github.com>
2018-12-30 21:13:49 +0300

    1.2

--- clever_cam_calibration/clevercamcalib.py
+++ clever_cam_calibration/clevercamcalib.py
@@ -2,13 +2,7 @@ import numpy as np
 import cv2
 import glob
 import yaml
-from cv_bridge import CvBridge
-from sensor_msgs.msg import Image
-import rospy
-
-def image_callback(data):
-    cv_image = bridge.imgmsg_to_cv2(data, 'bgr8')  # OpenCV image
-    gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY, dstCn=0)
+import urllib.request
 
 def set_camera_info(chessboard_size, square_size, images_topic, destination):
     if chessboard_size is not None:
@@ -56,43 +50,22 @@ def set_camera_info(chessboard_size, square_size, images_topic, destination):
 
 
 def get_undistorted_image(image, camera_info):
-    img = cv2.imread(image)
     file = yaml.load(open(camera_info))
     matrix = file['matrix']
     distortions = file['distortion']
-    h, w = img.shape[:2]
+    h, w = image.shape[:2]
     newcameramtx, roi = cv2.getOptimalNewCameraMatrix(matrix, distortions, (w, h), 1, (w, h))
-    dst = cv2.undistort(img, matrix, distortions, None, newcameramtx)
+    dst = cv2.undistort(image, matrix, distortions, None, newcameramtx)
     x, y, w, h = roi
     dst = dst[y:y + h, x:x + w]
     return dst
 
 
-def calibrate(chessboard_size, square_size, __restarted = False):
-    if not __restarted:
-        print("Calibration started!")
-        #camera = cv2.VideoCapture(0)
-        global bridge
-        bridge = CvBridge()
-        image_sub = rospy.Subscriber('main_camera/image_raw', Image, image_callback, queue_size=1)
-        print(type(image_sub))
-    if chessboard_size is not None:
-        size = list(map(int, chessboard_size.split("x")))
-        if len(size) == 2:
-            length = size[0]
-            width = size[1]
-        else:
-            print("Incorrect chessboard_size")
-            quit()
-    else:
-        print("Incorrect chessboard_size")
-        quit()
-    if square_size is None:
-        print("Incorrect square_size")
-        quit()
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, square_size, 0.001)
-    objp = np.zeros((length * width, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:length, 0:width].T.reshape(-1, 2)
+def calibrate():
+    print("Calibration started!")
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    objp = np.zeros((6 * 7, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
     objpoints = []
     imgpoints = []
     gray_old = None
@@ -100,14 +73,14 @@ def calibrate(chessboard_size, square_size, __restarted = False):
     print("Commands:")
     print("help, catch (key: Enter), delete, restart, stop, finish")
     while True:
-        command = raw_input()
+        command = input()
         if command == "catch" or command == "":
             print("---")
-            #return_value, image = camera.read()
-            #req = urllib.request.urlopen('http://192.168.11.1:8080/snapshot?topic=/main_camera/image_raw')
-            #arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-            #image = cv2.imdecode(arr, -1)
-            ret, corners = cv2.findChessboardCorners(image_sub, (length, width), None)
+            req = urllib.request.urlopen('http://192.168.11.1:8080/snapshot?topic=/main_camera/image_raw')
+            arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+            image = cv2.imdecode(arr, -1)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
             if ret:
                 objpoints.append(objp)
                 corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
@@ -143,9 +116,7 @@ def calibrate(chessboard_size, square_size, __restarted = False):
                 else:
                     print("Not enough images, now "+str(len(objpoints))+" (10 required)")
             elif command == "restart":
-                print("chessboard size: "+str(chessboard_size)+" "+"square size: "+str(square_size))
-                camera.release()
-                calibrate(chessboard_size, square_size, True)
+                calibrate()
                 break
         elif len(command.split()) == 2 and command.split()[0] == "help":
             command = command.split()[1]
@@ -165,20 +136,5 @@ def calibrate(chessboard_size, square_size, __restarted = False):
                 print("If there are 10 photos or more, calibration coefficients will be saved in present directory as 'camera_info.yaml' ")
             else:
                 print("unknown command")
-        elif command.split()[0] == "restart":
-            command = command.split()
-            new_size = chessboard_size
-            new_sq_size = square_size
-            if len(command) == 2:
-                new_size = command[1]
-            elif len(command) == 3:
-                new_size == command[1]
-                new_sq_size == command[2]
-            elif len(command) != 1:
-                print("incorrect arguments for 'restart' command")
-            camera.release()
-            calibrate(new_size, new_sq_size)
-            break
         else:
             print("Unknown command")
-    camera.release()
