8763289ccda64c28d6580c690732237016e937f0
tinderad <44276006+tinderad@users.noreply.github.com>
2019-02-09 19:15:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='clever_cam_calibration',
     version='1.0',
     packages= ['clever_cam_calibration'],
-    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
     entry_points={'console_scripts':['calibrate_cam = clever_cam_calibration.clevercamcalib:calibrate_command',
                                      'calibrate_cam_ex = clever_cam_calibration.clevercamcalib:calibrate_ex_command']}
 )
